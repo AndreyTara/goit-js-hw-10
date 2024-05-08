8fc9d2bbff7899baffddcda4597e0d12743f16fc
AndreyTara <explorituse@gmail.com>
2024-05-08 23:14:29 +0300

    'adds-comment-timer'

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+//npm install -g modernizr
+
 # Vanilla App Template
 
 Цей проект було створено за допомогою Vite. Для знайомства та налаштування
--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,28 +1,135 @@
-// // Описаний в документації
-// import flatpickr from "flatpickr";
-// // Додатковий імпорт стилів
-// import "flatpickr/dist/flatpickr.min.css";
+//додавання змінних для дом елемента кнопка запуску
+const buttonDom = document.querySelector('.section button[data-start]');
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+//додавання змінних для дом елементів відображення елментів
+const timerDom = document.querySelectorAll('.timer .field span.value');
+
+// затемененя кнопки для розуміння неактивності кнопки
+buttonDom.classList.add('inactive');
+
+//визначення змінних !!!!!!!!!!!!!!!!!!!!
 let userSelectedDate;
-onClose();
-function onClose() {
-  if (false) {
-    //button Start inactive
-    window.alert('"Please choose a date in the future"');
-  } else if (true) {
-    //button Start active
+
+// визначення поточного дати та часу
+const curDate = new Date();
+
+// визначення змінної поточного шндентифікатора setInterval() для його видалення
+let outerItnervalId;
+
+//визначення змінної для прибрання навішування слухача при обранні тієїєж дати відліку
+let deltaDatePrev;
+
+// опції для flatpickr
+const options = {
+  enableTime: true, //відображення дати
+  time_24hr: true, // 24-часовий відображення
+  defaultDate: new Date(), //потчні дата на приклад як -  placeholder
+  minuteIncrement: 1, // встановлення часу минімальні зміни на 1 хвилину
+  onClose: closeCalendar, //визов функції після обрання дати та часу
+};
+
+// конфігурування flatpickr із підключенням опцій
+flatpickr('#datetime-picker', options);
+
+/**
+ * функція запуску відліку до обраної дати
+ * @param {Array} selectedDates формат обраноъ дати
+ * @returns
+ */
+function closeCalendar(selectedDates) {
+  // дельта дат обраної та поточної
+  let deltaDateCur = selectedDates[0].getTime() - curDate.getTime();
+  // перевірку на дати в минулому
+  if (deltaDateCur < 0) {
+    // відображення попееджувальноого напису
+    window.alert('Please choose a date in the future');
+    // кнопка отримує стиль класу'.inactive'
+    buttonDom.classList.add('inactive');
+    return;
+  }
+
+  // перевірка зміни обраної дати для виключення повторно обраної дати
+  if (deltaDateCur !== deltaDatePrev) {
+    // видалення стилю класу'.inactive'
+    buttonDom.classList.remove('inactive');
+    // попереднє значення дорівнює поточній дельті дат
+    deltaDatePrev = deltaDateCur;
+    //визов функції як аргумент додано дельту дат
+    iterate(deltaDatePrev);
   }
 }
 
+/**
+ * функція запуску відображення відліку до обраної дати у полі timerDom
+ * @param {Numder} value
+ */
+function iterate(deltaDate) {
+  let value = deltaDate;
+  buttonDom.addEventListener(
+    'click',
+    event => {
+      // Перевіряємо, чи існує інтервал перед тим як створювати новий старий треба стерти
+      if (outerItnervalId) {
+        clearInterval(outerItnervalId);
+      }
+      // завдання сетІнтревалу() для рахування value
+      let intervalId = setInterval(() => {
+        //пертворення value до обєкту із 4 параметрами день, час, хвилина, секунда
+        let objDateData = convertMs(value);
+        // відображення знаяень у span
+        addsDataToDom({ objDateData, timerDom });
+        //зменьшення значення value на 1000мікросекунд
+        value -= 1000;
+        //умова завершення циклу та стирання сетІнтервалу()
+        if (value < 0) {
+          console.log('Done');
+          clearInterval(intervalId);
+        }
+      }, 1000);
+      //зберегли значення сетІнтревалу() для його видалення після завдання нового сетІнтервалу()
+      outerItnervalId = intervalId;
+    },
+    { once: true } // працює лише 1 раз потім слухач зникає
+  );
+}
+
+/**
+ * функція відображення у елементах timerDom значеннь змінних у objDateData
+ * @param { Object} objDateData - формат дати у виглдяді об'єкту { days, hours, minutes, seconds }
+ * @param { NodeList} timerDom - timerDom - посилання на дом елементи в HTML
+ */
+function addsDataToDom({ objDateData, timerDom }) {
+  // ітерація обєкту objDateData
+  for (const item in objDateData) {
+    // ітерація timerDom
+    for (let i = 0; i < timerDom.length; i++) {
+      //якщо i-тий елемент із датасет ==''
+      if (timerDom[i].dataset[item] === '') {
+        // якщо значення timerDom.textContent не змінилося у objDateData
+        if (timerDom[i].textContent !== addsZero(objDateData[item])) {
+          // змінити значення timerDom якщо значення objDateData відповідне змінилося
+          timerDom[i].textContent = addsZero(objDateData[item]);
+        }
+      }
+    }
+  }
+}
+
+/**
+ * функція додавання "0" до значень менших за 10
+ * @param {Number} value - передане значення
+ * @returns String
+ */
+function addsZero(value) {
+  if (value < 10) return `0${value}`;
+  return `${value}`;
+}
+
+/**
+ * перетворення значень поточного часу у (ms) для отримання об'єкту із відповідними параметрами
+ * @param {NUmber} ms значення часу у мс
+ * @returns Object - { days, hours, minutes, seconds }
+ */
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -41,7 +148,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
