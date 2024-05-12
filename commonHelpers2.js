f3352905c8f214ee104fbb32a3ef71f283e063ec
AndreyTara <114803995+AndreyTara@users.noreply.github.com>
2024-05-12 10:22:48 +0000

    Deploying to gh-pages from @ AndreyTara/goit-js-hw-10@04dbf914da74ef70e3abe3b1bbff3ce869177e61 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-41e93212.js";document.querySelector(".form");const t=document.querySelectorAll(".form input"),e=["delay","fulfilled","rejected"],c={};for(const o of t)e.forEach(r=>{(o[r]===r||o.value===r)&&(c[delay]=o)});
+//# sourceMappingURL=commonHelpers2.js.map
